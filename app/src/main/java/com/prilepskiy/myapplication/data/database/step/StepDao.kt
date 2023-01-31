15facc940f579b86669b4203087b11d5ff2a4553
Prilepskiy_ae <alexey.prilepskiy@btb.dev>
2023-01-31 16:41:49 +0700

    add di

--- app/src/main/java/com/prilepskiy/myapplication/data/database/step/StepDao.kt
+++ app/src/main/java/com/prilepskiy/myapplication/data/database/step/StepDao.kt
@@ -1,8 +1,24 @@
 package com.prilepskiy.myapplication.data.database.step
 
 import androidx.room.Dao
+import androidx.room.Query
 import com.prilepskiy.myapplication.data.database.BaseDao
 import com.prilepskiy.myapplication.data.database.target.TargetEntity
+import kotlinx.coroutines.flow.Flow
+
 @Dao
 abstract class StepDao: BaseDao<StepEntity>() {
+
+    @Query("SELECT * FROM step_table")
+    abstract fun getAllSteps(): Flow<List<StepEntity>>
+
+    @Query("SELECT * FROM step_table WHERE titleTarget=:titleTarget")
+    abstract fun getStepFromTarget(titleTarget:String): Flow<List<StepEntity>>
+
+    @Query("SELECT * FROM step_table WHERE title=:title")
+    abstract fun getStep(title:String): Flow<List<StepEntity>>
+
+    @Query("DELETE FROM step_table WHERE title=:title")
+    abstract suspend fun deleteStep(title:String)
+
 }
--- app/src/main/java/com/prilepskiy/myapplication/data/database/target/TargetDao.kt
+++ app/src/main/java/com/prilepskiy/myapplication/data/database/target/TargetDao.kt
@@ -1,9 +1,18 @@
 package com.prilepskiy.myapplication.data.database.target
 
 import androidx.room.Dao
+import androidx.room.Query
 import com.prilepskiy.myapplication.data.database.BaseDao
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 abstract class TargetDao:BaseDao<TargetEntity>() {
 
+    @Query("SELECT * FROM target_table")
+    abstract fun getAllTargets(): Flow<List<TargetEntity>>
+    @Query("SELECT * FROM target_table WHERE title=:data")
+    abstract fun getTarget(data:String): Flow<List<TargetEntity>>
+
+    @Query("DELETE FROM target_table WHERE title=:data")
+    abstract suspend fun deleteTarget(data:String)
 }
