4f1817c9f280cbce019ad468bad834bd6b7c3d7a
Prilepskiy_ae <alexey.prilepskiy@btb.dev>
2023-02-10 08:09:17 +0700

    redis

--- app/src/main/java/com/prilepskiy/myapplication/ui/profile/ProfileViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.prilepskiy.myapplication.ui.profile
-
-import com.prilepskiy.myapplication.ui.base.BaseViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class ProfileViewModel@Inject constructor(): BaseViewModel() {
-}
